78ff3704f42cd11aa096a20c33dd8ae8d327dc64
Thomas Burton <twyburton@outlook.com>
2025-06-17 22:34:38 +0100

    first implementation

---
+++ receive/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+
+
+string data = "";
+
+Console.WriteLine("START");
+
+for (int i = 0; i < 500; i++)
+{
+    bool capsLock = Console.CapsLock;
+    bool numLock = Console.NumberLock;
+
+    if( capsLock ){
+        data += "1";
+    } else {
+        data += "0";
+    }
+    Thread.Sleep(2);
+}
+
+Console.WriteLine("END");
+
+Console.WriteLine(data);
---
+++ send/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+
+// Import keybd_event function from user32.dll
+[DllImport("user32.dll", SetLastError = true)]
+static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+// Constants for virtual key codes and event flags
+const int VK_CAPITAL = 0x14;
+const uint KEYEVENTF_EXTENDEDKEY = 0x1;
+const uint KEYEVENTF_KEYUP = 0x2;
+
+
+
+static void ToggleCapsLock()
+{
+    // Check current state
+    bool isCapsLockOn = Control.IsKeyLocked(Keys.CapsLock);
+
+    // Press Caps Lock key
+    keybd_event((byte)VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY, UIntPtr.Zero);
+
+    // Release Caps Lock key
+    keybd_event((byte)VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, UIntPtr.Zero);
+}
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+for (int i = 0; i < 50; i++)
+{
+    Console.WriteLine(Console.CapsLock);
+    // System.Windows.Forms.SendKeys.SendWait("{CAPSLOCK}")
+    ToggleCapsLock();
+    // Console.WriteLine(Console.CapsLock);
+    Thread.Sleep(2);
+}
